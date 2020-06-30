a42a65b610722fc03a2ecbf11ec47db32de50604
julianramirezch <julianramirezch1@gmail.com>
2020-06-30 17:41:11 -0500

    [imports]

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -5,6 +5,12 @@ import pep8
 import json
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 class TestFileStorageRequirements(unittest.TestCase):
