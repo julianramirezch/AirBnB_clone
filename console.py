0bb42db20b750a1060b3b44afb704927456ee6c7
julianramirezch <julianramirezch1@gmail.com>
2020-06-30 12:50:21 -0500

    [fix]: Imports

--- console.py
+++ console.py
@@ -2,7 +2,16 @@
 """Console command."""
 import cmd
 import sys
-from models import *
+import json
+import sys
+from models.base_model import BaseModel
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 class c_c:
