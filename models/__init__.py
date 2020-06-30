33d44e6553daf95cb54414011bc6e95a00cd32b6
julianramirezch <julianramirezch1@gmail.com>
2020-06-30 18:11:42 -0500

    [add]: Imports

--- models/__init__.py
+++ models/__init__.py
@@ -2,7 +2,12 @@
 """Read doc from model."""
 from .engine.file_storage import FileStorage
 from .base_model import BaseModel
-
+from .amenity import Amenity
+from .city import City
+from .place import Place
+from .review import Review
+from .state import State
+from .user import User
 
 storage = FileStorage()
 storage.reload()
