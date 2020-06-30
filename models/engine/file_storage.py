89615f45139c2f00607cc290f042d582879848e7
julianramirezch <julianramirezch1@gmail.com>
2020-06-30 16:19:37 -0500

    [add]: Comments

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,13 +2,13 @@
 """ Module: Storage """
 
 import json
-from models.base_model import BaseModel
-from models.user import User
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
+# from models.base_model import BaseModel
+# from models.user import User
+# from models.amenity import Amenity
+# from models.city import City
+# from models.place import Place
+# from models.review import Review
+# from models.state import State
 
 
 class FileStorage:
