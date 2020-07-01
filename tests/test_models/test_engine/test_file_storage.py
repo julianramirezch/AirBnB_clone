94bf59b25f91f23bb507bddf8c2994b4b53f9b1d
julianramirezch <julianramirezch1@gmail.com>
2020-06-30 19:22:31 -0500

    [test]

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,54 +1,54 @@
 #!/usr/bin/python3
-""" FileStorage unit testing """
-import unittest
-import pep8
+"""This is a class FileStorage"""
 import json
 from models.base_model import BaseModel
-from models.engine.file_storage import FileStorage
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
 from models.user import User
+from models.place import Place
+from models.city import City
+from models.state import State
+from models.review import Review
+from models.amenity import Amenity
 
 
-class TestFileStorageRequirements(unittest.TestCase):
-    """ Tests base documentation """
+class FileStorage:
+    """class FileStorage"""
+    __file_path = "file.json"
+    __objects = {}
 
-    @classmethod
-    def setUpClass(cls):
-        '''
-        is called with the class as the only argument
-        and must be decorated as a classmethod():
-        '''
-        pass
+    def all(self):
+        """returns the dictionary __objects
+        Returns:
+            __object dictionary
+        """
+        return FileStorage.__objects
 
-    @classmethod
-    def tearDownClass(cls):
-        '''
-        is called with the class as the only argument
-        and must be decorated as a classmethod():
-        '''
-        pass
+    def new(self, obj):
+        """sets in __objects the obj with key <obj class name>.id
+        Args:
+            obj (obj): object
+        """
+        FileStorage.__objects[type(obj).__name__ + "." + obj.id] = obj
 
-    def test_pep8(self):
-        """ Test that models/base.py conforms to PEP8 """
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['models/engine/file_storage.py'])
-        self.assertEqual(result.total_errors, 0, "Fix PEP8 Style")
+    def save(self):
+        """serializes __objects to the JSON file (path: __file_path)"""
+        a_dict = {}
+        for key in FileStorage.__objects:
+            a_dict[key] = FileStorage.__objects[key].to_dict()
+        with open(FileStorage.__file_path, mode="w",
+                  encoding="utf-8") as a_file:
+            json.dump(a_dict, a_file)
 
-    def test_pep8_base(self):
-        """ Test the test file xD """
-        pep8style = pep8.StyleGuide(quiet=True)
-        path_file = 'tests/test_models/test_engine/test_file_storage.py'
-        result = pep8style.check_files([path_file])
-        self.assertEqual(result.total_errors, 0, "Fix PEP8 Style")
+    def reload(self):
+        """deserializes the JSON file to __objects"""
+        try:
+            with open(FileStorage.__file_path, mode="r",
+                      encoding="utf-8") as a_file:
+                a_dict = json.load(a_file)
+            for key, value in a_dict.items():
+                FileStorage.__objects[key] = eval(value['__class__'])(**value)
+        except FileNotFoundError:
+            pass
 
-    def test_docstring(self):
-        """test if docstring"""
-        self.assertIsNotNone(FileStorage.__doc__)
-
-    def test_docmodule(self):
-        """ Tests module """
-        self.assertTrue(len(FileStorage.__doc__) >= 1)
+    def delete_obj(self):
+        """Deletes everything"""
+        FileStorage.__objects.clear()
